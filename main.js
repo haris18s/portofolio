ec58c8308f2e3468185aee20d3f2e2055e4376cb
Haris Spyridis <haris.spyridis@yahoo.com>
2026-07-15 12:03:46 +0300

    update portofolio

---
+++ main.js
@@ -0,0 +1,26 @@
+let a = ["Bioinformatics", "Computational Genomics", "Metagenomics", "Evolutionary Genomics"];
+let ref = document.getElementById("text");
+let ind = 0, cInd = 0;
+let remove = false;
+
+function typing() {
+    if (ind < a.length) {
+        let currentText = a[ind];
+        if (!remove && cInd < currentText.length) {
+            ref.textContent += currentText.charAt(cInd);
+            cInd++;
+            setTimeout(typing, 100);
+        } else if (remove && cInd >= 0) {
+            ref.textContent = currentText.substring(0, cInd);
+            cInd--;
+            setTimeout(typing, 100);
+        } else {
+            remove = !remove;
+            if (!remove) {
+                ind = (ind + 1) % a.length;
+            }
+            setTimeout(typing, 1000);
+        }
+    }
+}
+typing();
